5ce734cd240b10d05e397b4179465a6cbb5df22b
Muhammad Adnan Bashir <60353721+dani5598@users.noreply.github.com>
2026-06-02 14:35:42 +0500

    fix: replace GTM iframe inline style with Tailwind classes in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -268,7 +268,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             src="https://www.googletagmanager.com/ns.html?id=GTM-KMPSR8SX"
             height="0"
             width="0"
-            style={{ display: "none", visibility: "hidden" }}
+            className="hidden invisible"
           />
         </noscript>
         <ThemeProvider
